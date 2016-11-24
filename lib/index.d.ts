8a2be5b5ac17bc24697ce78708df028d0c36b57a
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:37:27 +0800

    发布组件

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import injectInstance from './inject-instance/inject-instance.component';
-import inject from './inject-decorator/inject-decorator.component';
-export default injectInstance;
-export { injectInstance, inject };
