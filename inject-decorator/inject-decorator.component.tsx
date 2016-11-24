4478fd6aeeae01137dc2bb0e43317385365b3eaf
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:10:32 +0800

    修复bug

--- inject-decorator/inject-decorator.component.tsx
+++ inject-decorator/inject-decorator.component.tsx
@@ -3,7 +3,7 @@ export default (injectName: string): any => (target: any, propertyKey: string, d
 
     // 加入一个标注变量
     if (!target['_injectDecorator__injectVariables']) {
-        target['_injectDecorator__injectVariables'] = propertyKey
+        target['_injectDecorator__injectVariables'] = [propertyKey]
     } else {
         target['_injectDecorator__injectVariables'].push(propertyKey)
     }
