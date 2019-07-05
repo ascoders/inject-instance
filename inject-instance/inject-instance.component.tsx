ae2a96361a24e0c0e9febd8578b7c02ba9f81c02
黄子毅 <576625322@qq.com>
2019-07-05 23:33:27 +0800

    fix bug

--- inject-instance/inject-instance.component.tsx
+++ inject-instance/inject-instance.component.tsx
@@ -16,23 +16,23 @@ export default (...classes: Array<any>) => {
     })
 
     // 遍历所有实例
-    instanceMap.forEach((eachInstance: any, key: string) => {
-        // 保存此实例注入的实例
-        const injectInstances: Array<any> = []
-        // 找到该实例的类
-        const instanceClass = classMap.get(key)
-        // 遍历这个类的注入实例类名
-        eachInstance['_injectDecorator__injectVariables'] && eachInstance['_injectDecorator__injectVariables'].forEach((injectVariableKey: string) => {
-            if (!instanceMap.get(eachInstance[injectVariableKey])) {
-                throw 'injectName: ' + eachInstance[injectVariableKey] + ' not found!'
-            }
+  instanceMap.forEach((eachInstance: any, key: string) => {
+    // 遍历这个类的注入实例类名
+    if (eachInstance[INJECT_DECORATOR]) {
+      eachInstance[INJECT_DECORATOR].forEach((injectVariableKey: string) => {
+        const className = eachInstance.__proto__[injectVariableKey];
+        if (!instanceMap.get(className)) {
+          throw Error(`injectName: ${className} not found!`);
+        }
 
-            // 把注入名改成实际注入对象
-            eachInstance[injectVariableKey] = instanceMap.get(eachInstance[injectVariableKey])
-        })
-        // 删除这个临时变量
-        delete eachInstance['_injectDecorator__injectVariables']
-    })
+        // 把注入名改成实际注入对象
+        eachInstance[injectVariableKey] = instanceMap.get(className);
+      });
+    }
+
+    // 删除这个临时变量
+    delete eachInstance[INJECT_DECORATOR];
+  });
 
     return instanceMap
 }
